367e667cb6fa032d5593aca68da86153816efbbb
Christine Fletcher <chrismis79@gmail.com>
2020-10-15 07:32:45 -0500

    added routes for details/home pages and working

--- clientinfo/src/App.js
+++ clientinfo/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
 
 import './App.css';
 
@@ -7,7 +11,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Client Information</h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/details'>Details</Link>
+          </li>
+        </ul>
       </header>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/details'>
+          <Details />
+        </Route>
+      </Switch>
     </div>
   );
 }
